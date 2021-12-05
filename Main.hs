686873c253c5200e53fe038f790a02ab3cbe5cf7
Moritz May <moritzmay@hotmail.de>
2021-12-05 20:35:18 +0100

    Typen und Valid - Funktion hinzugefügt

--- Main.hs
+++ Main.hs
@@ -1,12 +1,37 @@
 module Main where
 
-import Control.Monad (forever)
-import Sudoku (getSudokuField, makeField, makeSudoku, printSudoku)
+import Sudoku (Sudoku, istValide)
+
+easy :: Sudoku
+easy =
+  [ "2....1.38",
+    "........5",
+    ".7...6...",
+    ".......13",
+    ".981..257",
+    "31....8..",
+    "9..8...2.",
+    ".5..69784",
+    "4..25...."
+  ]
+
+finished :: Sudoku
+finished =
+  [ "125783649",
+    "796245183",
+    "483961572",
+    "857412936",
+    "219836457",
+    "364579821",
+    "541397268",
+    "632158794",
+    "978624315"
+  ]
 
 main :: IO ()
 main = do
-  let testInput = "111111111222222222333333333444444444555555555666666666777777777888888888999999999"
-  let x = makeSudoku testInput
-  printSudoku (makeSudoku testInput)
+  if istValide finished
+    then putStrLn "True"
+    else putStrLn "False"
 
 --forever (getLine >>= putStrLn)
--- Sudoku.hs
+++ Sudoku.hs
@@ -1,15 +1,54 @@
 module Sudoku where
 
-import Data.Bits (Bits (xor))
-import Data.Char (chr, digitToInt, ord)
+import Data.List (transpose)
 
-type FieldRow = (Int, Int, Int)
+--Typdeklarationen
+type Sudoku = Matrix Wert
 
-type Field = (FieldRow, FieldRow, FieldRow)
+type Matrix param = [Reihe param]
 
-type SudokuRow = (Field, Field, Field)
+type Reihe param = [param]
 
-type Sudoku = (SudokuRow, SudokuRow, SudokuRow)
+type Wert = Char
+
+--Globale Definitionen
+feldgroesse :: Int
+feldgroesse = 3
+
+werte :: [Wert]
+werte = ['1' .. '9']
+
+istLeer :: Wert -> Bool
+istLeer param = param == '~'
+
+--Hilfsfunktionen
+zeilen :: Matrix param -> [Reihe param]
+zeilen = id
+
+spalten :: Matrix param -> [Reihe param]
+spalten = transpose
+
+felder :: Matrix param -> [Reihe param]
+felder x = packAus (map spalten (packEin x))
+  where
+    packEin x1 = split (map split x1)
+    split x2 = aufteilen feldgroesse x2
+    packAus x3 = map concat (concat x3)
+
+aufteilen :: Int -> [param] -> [[param]]
+aufteilen by [] = []
+aufteilen by xs = take by xs : aufteilen by (drop by xs)
+
+--Validierung
+istValide :: Sudoku -> Bool
+istValide sudoku =
+  all hatKeineDuplikate (zeilen sudoku)
+    && all hatKeineDuplikate (spalten sudoku)
+    && all hatKeineDuplikate (felder sudoku)
+
+hatKeineDuplikate :: Eq param => [param] -> Bool
+hatKeineDuplikate [] = True
+hatKeineDuplikate (x : xs) = notElem x xs && hatKeineDuplikate xs
 
 charToInt :: Char -> Int
 charToInt '0' = 0
@@ -26,65 +65,4 @@ charToInt x = -1
 
 --Input
 
---Input - Vorgabe:
---  Alle Zahlen als String hintereinander weg
---  Feld 1-1, Feld 1-2, Feld 1-3, Feld 2-1 ... Feld 3-3, Feld 1-4, Feld 2-4, ... Feld 3-9, Feld 4-1, Feld 4-2 ...
-
---  1-1 | 1-2 | 1-3   1-4 | 1-5 | 1-6   1-7 | 1-8 | 1-9
---  2-1 | 2-2 | 2-3   2-4 | 2-5 | 2-6   2-7 | 2-8 | 2-9
---  3-1 | 3-2 | 3-3   3-4 | 3-5 | 3-6   3-7 | 3-8 | 3-9
-
---  4-1 | 4-2 | 4-3   4-4 | 4-5 | 4-6   4-7 | 4-8 | 4-9
---  5-1 | 5-2 | 5-3   5-4 | 5-5 | 5-6   5-7 | 5-8 | 5-9
---  6-1 | 6-2 | 6-3   6-4 | 6-5 | 6-6   6-7 | 6-8 | 6-9
-
---  7-1 | 7-2 | 7-3   7-4 | 7-5 | 7-6   7-7 | 7-8 | 7-9
---  8-1 | 8-2 | 8-3   8-4 | 8-5 | 8-6   8-7 | 8-8 | 8-9
---  9-1 | 9-2 | 9-3   9-4 | 9-5 | 9-6   9-7 | 9-8 | 9-9
-
-makeField :: String -> Field
-makeField s = do
-  let row1 = (charToInt (head s), charToInt (s !! 1), charToInt (s !! 2))
-  let row2 = (charToInt (s !! 3), charToInt (s !! 4), charToInt (s !! 5))
-  let row3 = (charToInt (s !! 6), charToInt (s !! 7), charToInt (s !! 8))
-  (row1, row2, row3)
-
-makeSudokuRow :: String -> SudokuRow
-makeSudokuRow x = do
-  let field1 = makeField (take 9 x)
-  let y = drop 9 x
-  let field2 = makeField (take 9 y)
-  let z = drop 9 y
-  let field3 = makeField (take 9 z)
-  (field1, field2, field3)
-
-makeSudoku :: String -> Sudoku
-makeSudoku x = do
-  let row1 = makeSudokuRow (take 27 x)
-  let y = drop 27 x
-  let row2 = makeSudokuRow (take 27 y)
-  let z = drop 27 y
-  let row3 = makeSudokuRow (take 27 z)
-  (row1, row2, row3)
-
 --Output
-
-getSudokuFieldRow :: FieldRow -> String -> String
-getSudokuFieldRow (x, y, z) s = [chr (ord '0' + x)] ++ "|" ++ [chr (ord '0' + y)] ++ "|" ++ [chr (ord '0' + z)]
-
-getSudokuField :: Field -> String -> String
-getSudokuField (x, y, z) s = getSudokuFieldRow x s ++ "\n" ++ getSudokuFieldRow y s ++ "\n" ++ getSudokuFieldRow z s
-
-printSudoku :: Sudoku -> IO ()
-printSudoku (((a1, b1, c1), (a2, b2, c2), (a3, b3, c3)), ((d1, e1, f1), (d2, e2, f2), (d3, e3, f3)), ((g1, h1, i1), (g2, h2, i2), (g3, h3, i3))) = do
-  putStrLn (getSudokuFieldRow a1 "" ++ "\t" ++ getSudokuFieldRow a2 "" ++ "\t" ++ getSudokuFieldRow a3 "")
-  putStrLn (getSudokuFieldRow b1 "" ++ "\t" ++ getSudokuFieldRow b2 "" ++ "\t" ++ getSudokuFieldRow b3 "")
-  putStrLn (getSudokuFieldRow c1 "" ++ "\t" ++ getSudokuFieldRow c2 "" ++ "\t" ++ getSudokuFieldRow c3 "")
-  putStrLn ""
-  putStrLn (getSudokuFieldRow d1 "" ++ "\t" ++ getSudokuFieldRow d2 "" ++ "\t" ++ getSudokuFieldRow d3 "")
-  putStrLn (getSudokuFieldRow e1 "" ++ "\t" ++ getSudokuFieldRow e2 "" ++ "\t" ++ getSudokuFieldRow e3 "")
-  putStrLn (getSudokuFieldRow f1 "" ++ "\t" ++ getSudokuFieldRow f2 "" ++ "\t" ++ getSudokuFieldRow f3 "")
-  putStrLn ""
-  putStrLn (getSudokuFieldRow g1 "" ++ "\t" ++ getSudokuFieldRow g2 "" ++ "\t" ++ getSudokuFieldRow g3 "")
-  putStrLn (getSudokuFieldRow h1 "" ++ "\t" ++ getSudokuFieldRow h2 "" ++ "\t" ++ getSudokuFieldRow h3 "")
-  putStrLn (getSudokuFieldRow i1 "" ++ "\t" ++ getSudokuFieldRow i2 "" ++ "\t" ++ getSudokuFieldRow i3 "")
